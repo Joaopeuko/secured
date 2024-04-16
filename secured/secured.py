7a8189d860c4118e17fdb837bdc3d68bcbff2811
Joao Paulo Euko <joaopeuko@gmail.com>
2024-04-16 16:23:07 +0000

    Clean code

--- secured/secured.py
+++ secured/secured.py
@@ -5,7 +5,7 @@ from secure import Secure
 from pathlib import Path
 
 class AttrDict(dict):
-    def __init__(self, *args, secure=True, **kwargs):
+    def __init__(self, *args, secure, **kwargs):
         super(AttrDict, self).__init__(*args, **kwargs)
         self.secure = secure
         for key, value in self.items():
@@ -23,11 +23,11 @@ class AttrDict(dict):
         super(AttrDict, self).__setattr__(key, value)
 
 class Secured:
-    def __init__(self, yaml_path: str | List[str] |  List[Tuple] = None) -> None:
+    def __init__(self, yaml_path: str | List[str] = None, secure=False) -> None:
 
-        self.yaml_files = self.load_yaml(yaml_path)
+        self.yaml_files = self.load_yaml(yaml_path=yaml_path, secure=secure)
 
-    def load_yaml(self, yaml_path: str | List[str] | List[Tuple]) -> None:
+    def load_yaml(self, yaml_path: str | List[str], secure) -> None:
         if yaml_path is None:
             return
 
@@ -35,24 +35,21 @@ class Secured:
         for path in yaml_paths:
             with open(path, 'r') as file:
                 file_data = yaml.safe_load(file)
-                file_name = Path(path).stem
-                setattr(self, file_name, AttrDict(file_data))
+                file_name = Path(path).stem.replace('-', '_')
+                setattr(self, file_name, AttrDict(file_data, secure=secure))
 
     def get(self, key: str, required: bool = False) -> Secure | None:
-
-        # Check YAML data first
-        yaml_value = self.yaml_files.get(key)
-        if yaml_value is not None:
-            return Secure(yaml_value)
 
         # Check environment variables
         env_value = os.getenv(key)
 
         if env_value is None:
             if required:
-                raise ValueError(f"Key '{key}' not found in the OS environment, the provider, or the YAML file.")
+                raise ValueError(f"Key '{key}' not found in the OS environment.")
 
         return Secure(env_value)
 
-    def get_yaml_file(self, path: str) -> dict:
-        return self.yaml_files.get(path)
+
+secured = Secured('config-secrets.yaml', secure=True)
+
+print(secured.config_secrets.test.test1.test2.test4)
