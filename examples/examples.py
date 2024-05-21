37490bd4382b6c0dda2739ba6e1faf059bd674f2
João Paulo Euko <50120357+Joaopeuko@users.noreply.github.com>
2024-05-21 09:00:09 +0100

    12 enhance secure class for string composition in f strings (#15)
    This PR adds `secured.compose()` to compose strings with `secrets`

--- examples/examples.py
+++ examples/examples.py
@@ -1,13 +1,29 @@
-from secured.attribute import AttrDict
 from secured.secured import Secured
-message = "🔒 <Data Secured> 🔒"
-secured = Secured('examples/config-secrets.yaml', secure=True, message=message)
 
-print(secured.config_secrets.name) # type: ignore
-print(secured.config_secrets["name"]) # type: ignore
+# Define the custom secure message
+MESSAGE = "🔒 <Data Secured> 🔒"
+CONFIG_PATH = "examples/config.yaml"
 
-ad = AttrDict(secure=True, message=message)
-ad['password'] = 'my_secret'
-print((ad.password))
-print((ad['password']))
-print(ad.password == "my_secret")
+# Protect a sensitive string
+DATABASE_URL = "mysql://{user}:{password}@localhost/dbname"
+secure = Secured(CONFIG_PATH, secure=True, message=MESSAGE)
+
+# Usage in code
+secure_database_url  = secure.compose(DATABASE_URL, user="guest", password="guest_password")
+print(secure_database_url)  # Output: 🔒 <Data Secured> 🔒
+print(secure_database_url._get_original()) # Careful! This will print the original data, do not use it.
+print(secure_database_url == "mysql://guest:guest_password@localhost/dbname")
+
+# Example secured object
+secured = Secured('examples/config.yaml', secure=True, message=MESSAGE)
+
+# Composing the auth header
+print("Original data:", secured.config.databases['db3']['connection']['host']._get_original())
+auth_header = secured.compose("Bearer {host}", host=secured.config.databases['db3']['connection']['host'])
+print(auth_header)  # Output: 🔒 <Data Secured> 🔒
+print(auth_header == "Bearer db-server.local")  # Output: True
+
+# Formatting the Secure object
+formatted_key = secured.compose("{host}{port}", host=secured.config.databases['db3']['connection']['host'], port=secured.config.databases['db3']['connection']['port'])
+print(formatted_key)  # Output: 🔒 <Data Secured> 🔒
+print(formatted_key == "db-server.local5432")  # Output: True
